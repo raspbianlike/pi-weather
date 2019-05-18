70544b12c0b36ffd163d00919999c4df5702f4d6
raspbianlike <raspbian@gmx.at>
2019-05-18 19:37:05 +0200

    add mcp class; adapt sensors

--- src/data.py
+++ src/data.py
@@ -1,20 +1,66 @@
 # -*- coding: utf-8 -*-
 # /usr/bin/python
+from spidev import SpiDev
+
 import os
 import time
 import rrdtool
 import random
-# import DHT22
+import Adafruit_DHT
 # import pigpio
 import math
 import json
-# from gpiozero import Button
+from gpiozero import Button
 
 from ftplib import FTP
 
 
 # pi = pigpio.pi()
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+basepath = "/home/pi/pi-weather"
 
+settings = json.load(open("{}/settings.json".format(basepath)))
+print
+print json.dumps(settings, indent=4)
+print
+dht22 = Adafruit_DHT.AM2302
+
+# windspeed_sensor.when_pressed = None
+
+radius = 7.0
+exact_spin_time = 0
+
+file_list = []
+
+files = [
+    "{}/temperature_humidity_day.png",
+    "{}/wind_speed_dir_day.png",
+    "{}/dewpoint_other_day.png",
+
+    "{}/temperature_humidity_week.png",
+    "{}/wind_speed_dir_week.png",
+    "{}/dewpoint_other_week.png",
+    # "{}/live_data.txt"
+]
+
+# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
+
+class MCP3008:
+    def __init__(self, bus = 0, device = 0):
+        self.bus, self.device = bus, device
+        self.spi = SpiDev()
+        self.open()
+ 
+    def open(self):
+        self.spi.open(self.bus, self.device)
+    
+    def read(self, channel = 0):
+        adc = self.spi.xfer2([1, (8 + channel) << 4, 0])
+        data = ((adc[1] & 3) << 8) + adc[2]
+        return data
+            
+    def close(self):
+        self.spi.close()
 
 class Air:  # fix this
     def __init__(self, temperature=0.0, humidity=0.0, dewpoint=0.0, vp=0.0, svp=0.0):
@@ -57,10 +103,7 @@ class Air:  # fix this
         return 6.1078 * math.pow(10, ((self._a() * T) / (self._b() + T)))
 
     def rh(self):
-        return self._rh
-
-    def rh(self, T, DT):
-        return 100 * self.SDD(DT) / self.SDD(T)
+       return self._rh
 
     def DD(self):
         return self.rh() / 100 * self.SDD(self._T)
@@ -74,10 +117,13 @@ class Air:  # fix this
 
 def update_dht():  # DHT 22
     global data
-    dht22.trigger()
+    #dht22.trigger()
+    
     time.sleep(0.1)
-    data["air_temperature"] = round(dht22.temperature(), 2)
-    data["humidity"] = round(dht22.humidity(), 2)
+    humidity, temperature = Adafruit_DHT.read_retry(dht22, settings["sensors"]["am2302"]["pin"])
+    print humidity, temperature
+    data["air_temperature"] = round(temperature, 2)
+    data["humidity"] = round(humidity, 2)
 
 
 anemometer_hits = 0
@@ -89,13 +135,20 @@ def anemometer_callback():
     print "{}".format(anemometer_hits)
 
 
+windspeed_sensor = Button(27)
+
+windspeed_sensor.when_pressed = anemometer_callback
+#windspeed_sensor.when_released = anemometer_callback
 def update_anemometer():  # GPIO Input H/L
     global exact_spin_time, anemometer_hits, data
+    halfed_hits = int(anemometer_hits / 2.0)
     exact_delta_time = time.time() - exact_spin_time
     exact_spin_time = time.time()
-
-    speed = (anemometer_hits * radius) / exact_delta_time
-
+    print "hits {}".format(anemometer_hits)
+    speed = (halfed_hits * radius) / exact_delta_time # cm/s
+    speed = (speed / 100) * 3.6 # m/s
+    print "windspeed {}".format(speed)
+    anemometer_hits = 0
     data["wind_speed"] = round(speed, 2)
 
 
@@ -108,7 +161,8 @@ def update_others():  # Dewpoint calculation, etc.
     global data
     air = Air(temperature=data["air_temperature"], humidity=data["humidity"])
     dewpoint = air.DP()
-    cloudbase = (data["air_temperature"] - dewpoint) * 125 + settings["baseheight"]  # TODO: move this to Air class
+    cloudbase = (data["air_temperature"] - dewpoint) * 125 + int(settings["baseheight"])  # TODO: move this to Air class
+    print cloudbase
     data["dewpoint"] = cloudbase  # Refactor naming
 
 
@@ -191,7 +245,7 @@ def update_database():
         )
     global data
     try:
-        rrdtool.update("/home/raspbian/wetterstation/data.rrd", "N:{:4f}:{:4f}:{:4f}:{:4f}:{:4f}".format(
+        rrdtool.update("{}/data.rrd".format(basepath), "N:{:4f}:{:4f}:{:4f}:{:4f}:{:4f}".format(
             data["air_temperature"],
             data["dewpoint"],
             data["humidity"],
@@ -250,47 +304,15 @@ def create_graph():
 
 data = {"air_temperature": 0.0, "dewpoint": 0.0, "humidity": 0.0, "wind_speed": 0, "wind_direction": 0}
 
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-# dht22 = DHT22.sensor(settings["sensors"]["dht22"]["pin"])
-dht22 = None
-
-# windspeed_sensor = Button(5)
-windspeed_sensor = None
-# windspeed_sensor.when_pressed = anemometer_callback
-# windspeed_sensor.when_pressed = None
-
-radius = 7.0
-exact_spin_time = 0
-
-file_list = []
-
-basepath = "/home/raspbian/PycharmProjects/Wetterstation"
-files = [
-    "{}/temperature_humidity_day.png",
-    "{}/wind_speed_dir_day.png",
-    "{}/dewpoint_other_day.png",
-
-    "{}/temperature_humidity_week.png",
-    "{}/wind_speed_dir_week.png",
-    "{}/dewpoint_other_week.png",
-    # "{}/live_data.txt"
-]
-settings = json.load(open("{}/settings.json".format(basepath)))
-print
-print json.dumps(settings, indent=4)
-print
-
-# -=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-=-
-
 if __name__ == "__main__":
     print "starting loop!"
     while True:
         try:
-            '''update_dht()
+            update_dht()
             update_anemometer()
             update_windirection()
             update_others()
-            '''
+            
             update_database()
             create_graph()
             execute_ftp()
